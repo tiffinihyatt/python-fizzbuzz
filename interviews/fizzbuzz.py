ef4a1c9906192f41927ff4ce26697272b5d1553a
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-03-17 22:53:25 -0500

    Pass test_works_with_15

--- interviews/fizzbuzz.py
+++ interviews/fizzbuzz.py
@@ -1,1 +1,31 @@
+# def fizzbuzz(number):
+
+#     answer = []
+
+#     for current_number in range(1, number + 1):
+#         if current_number % 15 == 0:
+#             answer.append("FizzBuzz")
+#         elif current_number % 3 == 0:
+#             answer.append("Fizz")
+#         elif current_number % 5 == 0:
+#             answer.append("Buzz")
+#         else:
+#             answer.append(str(current_number))
+    
+#     return answer
+
 def fizzbuzz(number):
+    # current_number = 15
+    answer = []
+
+    for current_number in range(1, number + 1):
+        if current_number % 3 == 0 and current_number % 5 == 0:
+            answer.append("FizzBuzz")
+        elif current_number % 3 == 0:
+            answer.append("Fizz")
+        elif current_number % 5 == 0:
+            answer.append("Buzz")
+        else:
+            answer.append(f"{current_number}")
+    
+    return answer
